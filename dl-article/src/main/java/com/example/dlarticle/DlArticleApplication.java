bc547d2ba24b177778081f46065494f4d986a1c4
이주성 <32183520@dankook.ac.kr>
2024-05-28 14:47:13 +0900

    프로젝트 초기 세팅

---
+++ dl-article/src/main/java/com/example/dlarticle/DlArticleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dlarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlArticleApplication.class, args);
+    }
+
+}
